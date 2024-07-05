f6607751099fea67b46c0a53c1351fe5f1a4edf7
root <hlahusain@gmail.com>
2024-07-06 00:00:40 +0200

    update nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ python /app/create_medical_records_index.py
 
 python /app/download_nltk_resources.py
 
-exec supervisord -c /etc/supervisor/conf.d/supervisord.conf -n
+exec service nginx start
 
-# docker run --gpus all  --entrypoint "" -it 19f002d88c69 /bin/bash
+exec supervisord -c /etc/supervisor/conf.d/supervisord.conf -n
